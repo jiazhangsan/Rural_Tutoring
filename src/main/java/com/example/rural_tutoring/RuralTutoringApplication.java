a0c573a5f5e1e7c36d5289c06d6475d508098913
jiashuhang <2372200346@qq.com>
2025-05-08 17:10:44 +0800

    初始化农村家教管理系统项目

---
+++ src/main/java/com/example/rural_tutoring/RuralTutoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.rural_tutoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuralTutoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuralTutoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rural_tutoring/RuralTutoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rural_tutoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuralTutoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
