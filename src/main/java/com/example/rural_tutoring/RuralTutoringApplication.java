b51c9d177a6ec2ef5e14376587d28674c799ecb6
jiashuhang <2372200346@qq.com>
2025-05-08 17:16:02 +0800

    hello World

--- src/main/java/com/example/rural_tutoring/RuralTutoringApplication.java
+++ src/main/java/com/example/rural_tutoring/RuralTutoringApplication.java
@@ -8,6 +8,9 @@ public class RuralTutoringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RuralTutoringApplication.class, args);
+        System.out.println(
+                "Hello World!"
+        );
     }
 
 }
